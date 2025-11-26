fb7ebd3a16ae122a1e4fcdd68d4b25c971537cf5
João Vitor Givisiez <joaogivisiez05@gmail.com>
2025-11-26 10:44:46 -0300

    Subindo arquivos essenciais do crud, controller, entity, exception, repository...

---
+++ back-end/demo/src/main/java/vitual/libary/demo/entity/Book.java
@@ -0,0 +1,68 @@
+package vitual.libary.demo.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+@Entity
+@Table(name = "books")
+public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank(message = "O título é obrigatório.")
+    private String titulo;
+
+    @NotBlank(message = "O autor é obrigatório.")
+    private String autor;
+
+    @NotNull(message = "O ano de publicação é obrigatório.")
+    private Integer anoPublicacao;
+
+    @NotBlank(message = "O ISBN é obrigatório.")
+    private String isbn;
+
+    public Book() {}
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public Integer getAnoPublicacao() {
+        return anoPublicacao;
+    }
+
+    public void setAnoPublicacao(Integer anoPublicacao) {
+        this.anoPublicacao = anoPublicacao;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+}
